500bbf882f30e7ac64801bd6c0eba822419e820f
Tim Cortesi <tcortesi@gmail.com>
2018-09-20 22:40:33 -0400

    Allow name settable

--- app/Scheduler.php
+++ app/Scheduler.php
@@ -8,7 +8,7 @@ class Scheduler extends Model
 {
   protected $table = 'scheduler';
 
-  protected $fillable = ['cron','service_instance_id','route'];
+  protected $fillable = ['cron','service_instance_id','route','name'];
   protected $casts = ['args' => 'object'];
 
 }
