53578c11318d29f73b9614e3b062dc7d79078bb8
Tim Cortesi <tcortesi@gmail.com>
2018-09-21 11:02:29 -0400

    Calculate 5 next runtimes for cron

--- app/Scheduler.php
+++ app/Scheduler.php
@@ -11,11 +11,17 @@ class Scheduler extends Model
 
   protected $fillable = ['cron','service_instance_id','route','name','args'];
   protected $casts = ['args' => 'object','last_response'=>'object'];
-  protected $appends = ['next_run'];
+  protected $appends = ['next_runtimes'];
 
-  public function getNextRunAttribute() {
+  public function getNextRuntimesAttribute() {
     $cron = CronExpression::factory($this->attributes['cron']);
-    return $cron->getNextRunDate()->format('Y-m-d H:i:s');
+    return [
+      $cron->getNextRunDate(null,1)->format('Y-m-d H:i:s'),
+      $cron->getNextRunDate(null,2)->format('Y-m-d H:i:s'),
+      $cron->getNextRunDate(null,3)->format('Y-m-d H:i:s'),
+      $cron->getNextRunDate(null,4)->format('Y-m-d H:i:s'),
+      $cron->getNextRunDate(null,5)->format('Y-m-d H:i:s'),
+    ];
   }
 
 }
