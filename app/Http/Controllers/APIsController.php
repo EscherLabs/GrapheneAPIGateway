6fd5fa77142d57e0306f2084aa9fd37db58906d4
Tim Cortesi <tcortesi@gmail.com>
2020-05-15 10:33:36 -0400

    Last try

--- app/Http/Controllers/APIsController.php
+++ app/Http/Controllers/APIsController.php
@@ -44,10 +44,10 @@ class APIsController extends Controller
 
     public function latest_version($api_id)
     {
-        $latest_version = $versions = APIVersion::select('id')
+        $latest_version = APIVersion::select('id')
             ->where('api_id',$api_id)
             ->orderby('created_at','desc')->first();
-        $api_version = APIVersion::where('api_id','=',$latest_version->id)->orderBy('created_at', 'desc')->first();
+        $api_version = APIVersion::where('id','=',$latest_version->id)->orderBy('created_at', 'desc')->first();
         if (!is_null($api_version)) {
             return $api_version;
         } else {
